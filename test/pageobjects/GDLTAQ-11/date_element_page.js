61685f97576b02c294235764934e2a2c97d34448
cstgarcia <ckstgarcia@gmail.com>
2023-04-29 00:45:10 -0700

    create cases for all the quick select list and add cases for dashboard tab

--- test/pageobjects/GDLTAQ-11/date_element_page.js
+++ test/pageobjects/GDLTAQ-11/date_element_page.js
@@ -1,13 +1,25 @@
 class DateElement {
+    get dashboardTab () {
+        return $('a[id="Dashboard-nav"]');
+    }
+
+    get dashboardHeader() {
+        return $('div[class="ctx-labels text-subtitle-2"]');
+    }
+
     get smartViewTab() {
         return $('a[id="SmartView-nav"]');
     }
 
-    get btnDateDropdown() {
+    get smartViewHeader() {
+        return $('div[class="z-smartview-header"]');
+    }
+
+    get btnQuickSelect() {
         return $('button[data-aut="scrubber-button"][type="button"]');
     }
 
-    get dateOptionsContainer() {
+    get quickSelectPanel() {
         return $('div[class="scroll-container"]');
     }
 
@@ -15,22 +27,55 @@ class DateElement {
         return $$('//div[@class="z-overflow-ellipsis v-list-item v-list-item--link theme--dark"]');
     }
 
+    async selectDashboardTab() {
+        (await this.dashboardTab).click();
+    }
+
     async selectSmartVTab() {
-        (await this.smartViewTab).click();
+        await this.smartViewTab.click();
     }
 
-    async selectDateDropdown() {
-        (await this.btnDateDropdown).click();
-    }
-
-    async select3days() {
-        (await this.quickSelectList[5]).click();
+    async selectBtnQuickSelect() {
+        await this.btnQuickSelect.click();
     }
 
     async selectToday() {
-        (await this.quickSelectList[0]).click();
+        await this.quickSelectList[0].click();
+    }
+    
+    async selectYesterday() {
+        await this.quickSelectList[1].click();
     }
 
+    async selectBusinessWeek() {
+        await this.quickSelectList[2].click();
+    }
+    async select30d() {
+        await this.quickSelectList[3].click();
+    }
+    async select1Week() {
+        await this.quickSelectList[4].click();
+    }
+    async select3days() {
+        await this.quickSelectList[5].click();
+    }
+    async select24h() {
+        await this.quickSelectList[6].click();
+    }
+    async select3h() {
+        await this.quickSelectList[7].click();
+    }
+    async select1h() {
+        await this.quickSelectList[8].click();
+    }
+
+    async textYesterday() {
+        const date = new Date();
+        const yesterdayFormat = 
+            `${date.getMonth() + 1}/${date.getDate() - 1}/${date.getFullYear()}`;
+        return yesterdayFormat;
+    }
+    
 }
 
 export default new DateElement();

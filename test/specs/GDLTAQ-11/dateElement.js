53c7b74dcd78d1aa2b76bcb4bf661b7f41cb7419
cstgarcia <ckstgarcia@gmail.com>
2023-05-03 08:36:16 -0700

    erase the repeat code and only have the Smart View Task

--- test/specs/GDLTAQ-11/dateElement.js
+++ test/specs/GDLTAQ-11/dateElement.js
@@ -1,6 +1,6 @@
 import LoginPage from '../../pageobjects/login/login_page.js';
 import DateElement from '../../pageobjects/GDLTAQ-11/date_element_page.js';
-
+ 
 describe('Validate Time Range Controls from SmartView tab within Zenoss platform', async () => {
 
     it('should login success page', async () => {
@@ -23,6 +23,30 @@ describe('Validate Time Range Controls from SmartView tab within Zenoss platform
         expect(await DateElement.quickSelectPanel).toBeDisplayed();
     });
 
+
+/*    
+describe('Validate Time Range Controls from Dashboard tab', async () => {
+    it('should login success page', async () => {
+        await LoginPage.open();
+        await LoginPage.login('admin@elm1403-test.com', 'D0gP0und!');
+    });
+    
+    it('should handle alert message', async () => {
+        await LoginPage.alertMessage();
+    });
+    
+    it('should go to Dashboard tab', async () => {
+        await DateElement.selectDashboardTab();
+        expect(await DateElement.dashboardHeader).toHaveTextContaining('Dashboards');
+        browser.pause(2000);
+    });
+    
+    it('should display the quick select panel', async () => {
+        await DateElement.selectBtnQuickSelect();
+        await DateElement.quickSelectPanel.waitForDisplayed();
+        expect(await DateElement.quickSelectPanel).toBeDisplayed();
+    });
+ */
     it('should select Today option from the quick select list', async () => {
         await DateElement.quickSelectPanel.scrollIntoView();
         // for(let i = 0; i < await DateElement.quickSelectList.length; i++) {
@@ -102,90 +126,5 @@ describe('Validate Time Range Controls from SmartView tab within Zenoss platform
 
 });
 
-describe('Validate Time Range Controls from Dashboard tab', async () => {
 
-    it('should go to Dashboard tab', async () => {
-        await DateElement.selectDashboardTab();
-        expect(await DateElement.dashboardHeader).toHaveTextContaining('Dashboards');
-    });
 
-    it('should display the quick select panel', async () => {
-        await DateElement.selectBtnQuickSelect();
-        await DateElement.quickSelectPanel.waitForDisplayed();
-        expect(await DateElement.quickSelectPanel).toBeDisplayed();
-    });
-
-    it('should select Today option from the quick select list', async () => {
-        await DateElement.selectToday();
-        const today = new Date();
-        console.log('---->>>>> today in dropdown: ', await DateElement.btnQuickSelect.getText());
-        console.log('*** text today from JS****: ',today);
-        expect(await DateElement.btnQuickSelect).toHaveTextContaining(today);    
-    });
-
-    it('should select Yesterday option', async () => {
-        await DateElement.selectBtnQuickSelect();
-        await DateElement.quickSelectPanel.scrollIntoView();
-        await DateElement.selectYesterday();
-        console.log('---->>>>> yesterday in dropdown: ', await DateElement.btnQuickSelect.getText());
-        console.log('****yesterday in method textYesterday****: ', await DateElement.textYesterday());
-        expect(await DateElement.btnQuickSelect).toHaveTextContaining(await DateElement.textYesterday()); 
-    });
-
-    it('should select Business Week option', async () => {
-        await DateElement.selectBtnQuickSelect();
-        await DateElement.quickSelectPanel.scrollIntoView();
-        await DateElement.selectBusinessWeek();
-        console.log('---->>>>> BWeek in dorpdown: ', await DateElement.btnQuickSelect.getText());
-        expect(await DateElement.btnQuickSelect).toHaveTextContaining('Past hours minutes'); 
-    });
-
-    it('should select 30 days option', async () => {
-        await DateElement.selectBtnQuickSelect();
-        await DateElement.quickSelectPanel.scrollIntoView();
-        await DateElement.select30d();
-        console.log('---->>>>> 3 d: ', await DateElement.btnQuickSelect.getText());
-        expect(await DateElement.btnQuickSelect).toHaveTextContaining('Past 4 weeks 2 days');    
-    });
-
-    it('should select 1 week option', async () => {
-        await DateElement.selectBtnQuickSelect();
-        await DateElement.quickSelectPanel.scrollIntoView();
-        await DateElement.select1Week();
-        console.log('---->>>>> 1 week from dropdown: ', await DateElement.btnQuickSelect.getText());
-        expect(await DateElement.btnQuickSelect).toHaveTextContaining('Past 1 week');   
-    });
-
-    it('should select 3 days option', async () => {
-        await DateElement.selectBtnQuickSelect();
-        await DateElement.quickSelectPanel.scrollIntoView();
-        await DateElement.select3days();
-        console.log('---->>>>> 3 d: ', await DateElement.btnQuickSelect.getText());
-        expect(await DateElement.btnQuickSelect).toHaveTextContaining('Past 3 days');   
-    });
-
-    it('should select 24 hours option', async () => {
-        await DateElement.selectBtnQuickSelect();
-        await DateElement.quickSelectPanel.scrollIntoView();
-        await DateElement.select24h();
-        console.log('---->>>>> 24h from dropdown: ', await DateElement.btnQuickSelect.getText());
-        expect(await DateElement.btnQuickSelect).toHaveTextContaining('Past 24 hours');
-    });
-
-    it('should select 3 hours option', async () => {
-        await DateElement.selectBtnQuickSelect();
-        await DateElement.quickSelectPanel.scrollIntoView();
-        await DateElement.select3h();
-        console.log('---->>>>> 3h from dropdown: ', await DateElement.btnQuickSelect.getText());
-        expect(await DateElement.btnQuickSelect).toHaveTextContaining('Past 3 hours');
-    });
-
-    it('should select 1 hour option', async () => {
-        await DateElement.selectBtnQuickSelect();
-        await DateElement.quickSelectPanel.scrollIntoView();
-        await DateElement.select1h();
-        console.log('---->>>>> 1h from dropdown: ', await DateElement.btnQuickSelect.getText());
-        expect(await DateElement.btnQuickSelect).toHaveTextContaining('Past 1 hour');
-    });
-
-});
